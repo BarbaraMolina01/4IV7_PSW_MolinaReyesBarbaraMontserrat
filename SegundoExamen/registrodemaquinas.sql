25de8e8110d693842a0eceee5db1bd0c9cc9e969
Hector Molina Granados <hmolina@nube.sep.gob.mx>
2022-04-22 22:59:10 -0500

    2ExamenPracticoPSW

---
+++ SegundoExamen/registrodemaquinas.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: registrodemaquinas
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumno` (
+  `bol_al` int NOT NULL AUTO_INCREMENT,
+  `nom_al` varchar(30) NOT NULL,
+  `appat_al` varchar(30) NOT NULL,
+  `apmat_al` varchar(30) NOT NULL,
+  `img_al` varchar(30) NOT NULL,
+  `grupo_al` varchar(30) NOT NULL,
+  `turno_al` varchar(10) NOT NULL,
+  PRIMARY KEY (`bol_al`),
+  KEY `nom_al` (`nom_al`),
+  KEY `appat_al` (`appat_al`)
+) ENGINE=InnoDB AUTO_INCREMENT=2122458979 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno`
+--
+
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data` (
+  `id_data` int NOT NULL AUTO_INCREMENT,
+  `id_lab` int NOT NULL,
+  `bol_al` int NOT NULL,
+  PRIMARY KEY (`id_data`),
+  KEY `id_lab` (`id_lab`),
+  KEY `bol_al` (`bol_al`),
+  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`id_lab`) REFERENCES `laboratorio` (`id_lab`),
+  CONSTRAINT `data_ibfk_2` FOREIGN KEY (`bol_al`) REFERENCES `alumno` (`bol_al`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data`
+--
+
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `laboratorio`
+--
+
+DROP TABLE IF EXISTS `laboratorio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laboratorio` (
+  `id_lab` int NOT NULL AUTO_INCREMENT,
+  `nom_lab` varchar(4) NOT NULL,
+  `id_uni` int NOT NULL,
+  PRIMARY KEY (`id_lab`),
+  KEY `id_uni` (`id_uni`),
+  CONSTRAINT `laboratorio_ibfk_1` FOREIGN KEY (`id_uni`) REFERENCES `unidad` (`id_uni`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laboratorio`
+--
+
+LOCK TABLES `laboratorio` WRITE;
+/*!40000 ALTER TABLE `laboratorio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `laboratorio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reporte`
+--
+
+DROP TABLE IF EXISTS `reporte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reporte` (
+  `id_rep` int NOT NULL AUTO_INCREMENT,
+  `fecha_rep` date NOT NULL,
+  `mac_rep` varchar(12) NOT NULL,
+  `des_rep` varchar(35) NOT NULL,
+  `id_data` int NOT NULL,
+  PRIMARY KEY (`id_rep`),
+  KEY `id_data` (`id_data`),
+  CONSTRAINT `reporte_ibfk_1` FOREIGN KEY (`id_data`) REFERENCES `data` (`id_data`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reporte`
+--
+
+LOCK TABLES `reporte` WRITE;
+/*!40000 ALTER TABLE `reporte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reporte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unidad`
+--
+
+DROP TABLE IF EXISTS `unidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `unidad` (
+  `id_uni` int NOT NULL AUTO_INCREMENT,
+  `nom_uni` varchar(60) NOT NULL,
+  PRIMARY KEY (`id_uni`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unidad`
+--
+
+LOCK TABLES `unidad` WRITE;
+/*!40000 ALTER TABLE `unidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unidad` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-19 18:44:40
---
+++ SegundoExamen/web/media/logica.js
@@ -0,0 +1,24 @@
+function validarbol(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado === 8) return true;
+    var patron = /[1234567890]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function validardata(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado === 8 || teclado === 32) return true;
+    var patron = /[q w e r t y u i o p a s d f g h j k l ñ z x c v b n m Q W E R T Y U I O P A S D F G H J K L Ñ Z X C V B N M]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function validarmac(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado === 8) return true;
+    var patron = /[1 2 3 4 5 6 7 8 9 0 q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M -]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
